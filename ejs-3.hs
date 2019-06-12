f472bb93459e01e419523e73cf26c001e5816c79
azohc <juanchozass@gmail.com>
2019-06-12 22:13:32 +0200

    pracstart

--- ejs-3.hs
+++ ejs-3.hs
@@ -63,6 +63,114 @@ listaCjto (Cjto c) = c
 
 
 -- 4. Define un tipo para representar matrices de n´umeros reales. 
-data Matriz m = 
 -- Escribe una funci´on que calcule la transpuesta de una matriz rectangular dada. 
 -- Escribe una funci´on para calcular la operaci´on de suma de matrices.
+
+
+-- 5. Dada la declaraci´on:
+data Temp = Kelvin Float | Celsius Float | Fahrenheit Float deriving Show
+-- escribe una funci´on para realizar conversiones de una escala a otra 
+toCelsius :: Temp -> Float
+toCelsius (Kelvin k) = k-273.15
+toCelsius (Fahrenheit f) = (f-32)/(9/5)
+toCelsius (Celsius c) = c
+
+toFahrenheit :: Temp -> Float
+toFahrenheit (Kelvin k) = ((9/5)*(k-273.15))+32
+toFahrenheit (Celsius c) = ((9/5)*c)+32
+toFahrenheit (Fahrenheit f) = f
+
+toKelvin :: Temp -> Float
+toKelvin (Kelvin k) = k
+toKelvin (Celsius c) = c+273.15
+toKelvin (Fahrenheit f) = (5/9)*(f-32)+273.15
+
+normalizeKelvin :: Temp -> Temp
+normalizeKelvin = Kelvin . toKelvin
+
+normalizeCelsius :: Temp -> Temp
+normalizeCelsius = Celsius . toCelsius
+
+normalizeFahrenheit :: Temp -> Temp
+normalizeFahrenheit = Fahrenheit . toFahrenheit
+
+
+-- y otra para determinar la escala en la que est´a representada una temperatura. 
+
+getScale :: Temp -> String
+getScale (Fahrenheit f) = "Fahrenheit"
+getScale (Celsius c) = "Celsius"
+getScale (Kelvin k) = "Kelvin"
+
+-- El nuevo tipo tiene que ser instancia de las clases Ord y Eq. 
+-- Define adecuadamente los m´etodos compare y == para la nueva estructura de datos
+
+instance Eq Temp where
+    t == t' = toKelvin t == toKelvin t'
+
+instance Ord Temp where
+    t `compare` t' = (toKelvin t) `compare` (toKelvin t')
+
+
+-- 6. Declara adecuadamente un tipo de datos para representar ´arboles binarios de b´usqueda
+-- con valores en los nodos pero no en las hojas. Programa en Haskell la ordenaci´on de una
+-- lista por el algoritmo treeSort, consistente en ir colocando uno a uno los elementos de la
+-- lista en un ´arbol binario de b´usqueda inicialmente vac´ıo. 
+-- A continuaci´on devuelve la lista resultante de recorrer el ´arbol en inOrden.
+
+data Tree a = 
+    Node {
+        value :: a,
+        left :: (Tree a),
+        right :: (Tree a)
+    }
+    | Leaf deriving (Eq, Show)
+
+insert :: Ord a => a -> Tree a -> Tree a
+insert x Leaf = Node x Leaf Leaf
+insert x (Node v l r) 
+    | x == v    = Node x l r 
+    | x < v     = Node v (insert x l) r
+    | otherwise = Node v l (insert x r) 
+
+inOrden :: Ord a => Tree a -> [a]
+inOrden Leaf = []
+inOrden (Node v Leaf Leaf) = [v]
+inOrden (Node v l r) = inOrden l ++ [v] ++ inOrden r
+    
+
+treeSort :: Ord a => [a] -> [a]
+treeSort [] = []
+treeSort l  = inOrden (foldr insert Leaf l)
+
+
+-- 7. Escribe una funci´on adivina n para jugar a adivinar un n´umero. 
+-- Debe pedir que el usuario introduzca un n´umero hasta que acierte con el valor de n. 
+-- Devuelve mensajes de ayuda indicando si el n´umero introducido es menor o 
+-- mayor que el n´umero n a adivinar. 
+-- Observaque el tipo de la funci´on ser´a adivina :: Int -> IO ().
+
+adivina :: Int -> IO ()
+adivina n = do
+        putStrLn "introduce un numero: " 
+        input <- getLine
+        let n' = (read input :: Int)
+        
+        if n' == n then
+            putStrLn ("enhorabuena, el numero es " ++ (show n')) 
+        else if n' > n then do
+            putStrLn ("el numero es menor que " ++ (show n'))
+            adivina n
+        else do
+            putStrLn ("el numero es mayor que " ++ (show n'))
+            adivina n
+
+
+
+            
+    -- foldl f x [y1, y2, .. yk]
+    -- f .. (f (f x y1) y2) .. yk
+
+    -- foldr f x [y1, y2, .. yk]
+    -- f y1 (f y2 (..(f yk x)..))
+    
--- logica_proposicional.hs
+++ logica_proposicional.hs
@@ -1,2 +1,8 @@
+-- Juan Chozas Sumbera 
+
 type Var = String -- nombres de variables
-data FProp = V Var | No FProp | Y FProp FProp | O FProp FProp | Si FProp FProp | Sii FProp FProp
+data FProp = V Var | No FProp | Y FProp FProp 
+        | O FProp FProp | Si FProp FProp | Sii FProp FProp deriving Show
+
+-- f1 = !p -> (p -> (q v !q))
+-- f1 = Si (No (V "p")) (Si (V "p") (Y (V "q") (No (V "q"))))
